e5a9399266926d94f306c9d59e845bfc38b35a41
lizhaoguang <lizhaoguang@dangdang.com>
2016-07-01 18:00:54 +0800

    Stream 数据结构完成到 练习 5.13 的 zipAll

--- src/main/kotlin/me/mx1700/kotlinfunctional/Stream.kt
+++ src/main/kotlin/me/mx1700/kotlinfunctional/Stream.kt
@@ -11,11 +11,18 @@ object Empty : Stream<Nothing> {
     override fun toString() = "empty"
 }
 
+var count = 0;
 data class Cons<out T>(private val h: () -> T, private val t: () -> Stream<T>) : Stream<T> {
     override val head by lazy { h() }
     val tail by lazy { t() }
-
+    init {
+        count++
+    }
     override fun toString() = "${this.head}, ..."
+
+    fun finalize() {
+        count--
+    }
 }
 
 fun <T>Stream<T>.toList(): List<T> = when {
@@ -111,6 +118,78 @@ fun <T, R>Stream<T>.flatMap(f: (a: T) -> Stream<R>): Stream<R> = this.foldRight<
 fun <T, R>Stream<T>.flatMap2(f: (a: T) -> Stream<R>): Stream<R> = this.foldRight<T, Stream<R>>(Empty, { a, l -> f(a).foldRight2({ l() }, { a, l -> Cons( { a }, { l() })}) })
 fun <T>Stream<T>.find(f: (T) -> Boolean): T? = this.filter(f).head()
 
+/**
+ * 产生一个无限流
+ */
+fun <T>constant(a: T): Stream<T> {
+    var s14: Stream<T>? = null
+    s14 = Cons({ a }, { s14?: Empty })
+    return s14
+}
+
+/**
+ * 生成一个从 n 开始的无限流
+ */
+fun from(start: Int): Stream<Int> = Cons({ start }, { from(start + 1) })
+
+/**
+ * 斐波那契数列
+ */
+fun fibonacci(a: Int = 0, b: Int = 1): Stream<Int> = Cons({ a }, { Cons( { b }, { fibonacci(a + b, a + 2 * b) }) })
+
+/**
+ * 根据初始状态和生成下一个状态和值的函数，产生一个无限流
+ */
+fun <T, R>unfold(first: T, f: (T) -> Pair<T, R>?): Stream<R> {
+    val fr = f(first)
+    if (fr != null) {
+        val (t, r) = fr
+        return Cons({ r }, { unfold(t, f) })
+    } else {
+        return Empty
+    }
+}
+
+/**
+ * 用 unfold 实现斐波那契数列
+ */
+fun fibs() = unfold(0 to 1, { i ->  i.second to i.first + i.second to i.first  })
+fun from2(start: Int) = unfold(start, { i -> i + 1 to i })
+fun <T>constant2(a: T): Stream<T> = unfold(a, { i -> a to a })
+fun <T, R>Stream<T>.map2(f: (T) -> R): Stream<R> = unfold(this, { i -> when(i) {
+    is Cons -> i.tail to f(i.head)
+    else -> null
+} })
+fun <T>Stream<T>.take2(n: Int): Stream<T> = unfold(this to n, { s ->
+    val (stream, i) = s
+    when {
+        stream is Cons && i > 0 -> stream.tail to i - 1 to stream.head
+        else -> null
+    }
+})
+fun <T>Stream<T>.takeWhile3(f: (T) -> Boolean): Stream<T> = unfold(this, { s ->
+    when {
+        s is Cons && f(s.head) -> s.tail to s.head
+        else -> null
+    }
+})
+fun <T, R>Stream<T>.zipWith(l: Stream<T>, f: (a: T, b: T) -> R): Stream<R> = unfold(this to l, { s ->
+    val (s1, s2) = s
+    when {
+        s1 is Cons && s2 is Cons -> s1.tail to s2.tail to f(s1.head, s2.head)
+        else -> null
+    }
+})
+fun <T, T2>Stream<T>.zipAll(l: Stream<T2>): Stream<Pair<T?, T2?>> = unfold(this to l, { s ->
+    val (s1, s2) = s
+    when {
+        s1 is Cons && s2 is Cons -> s1.tail to s2.tail to (s1.head to s2.head)
+        s1 is Cons -> s1.tail to s2 to (s1.head to s2.head)
+        s2 is Cons -> s1 to s2.tail to (s1.head to s2.head)
+        else -> null
+    }
+});
+
 
 fun main(args: Array<String>) {
     //val stream = Cons({ 1 }, { Cons({ 2 }, { Empty }) })
@@ -142,10 +221,37 @@ fun main(args: Array<String>) {
 //    val s11 = streamOf(1,2,3).append(streamOf(4,5,6))
 //    println(s11.toList())
 
-    //val s12 = streamOf(1,2,3,4,5).flatMap2 { streamOf(it, it * 2) }
-    //println(s12.toList())
+//    val s12 = streamOf(1,2,3,4,5).flatMap2 { streamOf(it, it * 2) }
+//    val l = streamOf(1,2,3,4,5).flatMap2 { streamOf(it, it * 2) }.toList()
+//    val s13 = streamOf(1,2,3,4).find { it == 3 }
 
-    val s13 = streamOf(1,2,3,4).find { it == 3 }
+    //无法在初始化的时候自己引用自己
+    //var s: Stream<Int> = Cons<Int>({ 1 }, { s })
+
+//    var s14: Stream<Int>? = null
+//    s14 = Cons({ 1 }, { s14?: Empty })
+//
+//    println(s14.map { it + 1 }.take(10).toList())
+//    println(constant("aaa").take(3).toList())
+
+    //println(from(1).filter { it % 7 == 0 }.take(5).toList())
+
+//    println(fibonacci().take(10).toList())
+//    println(unfold(0, { i ->
+//        if (i < 26)
+//            i + 1 to (i + 97).toChar()
+//        else
+//            null
+//    }).take(100).toList())
+
+    //println(fibs().take(10).toList())
+    //println(from2(10).take(10).toList())
+    //println(streamOf(1,2,3,4,5).map2 { it * 2 }.toList())
+    //println(streamOf(1,2,3,4,5).take2(3).toList())
+    //println(streamOf(1,2,3,4,5,6).takeWhile3 { it < 3 }.toList())
+    //println(streamOf(1,2,3,4,5).zipWith(streamOf(10,11,12),{ a, b -> a + b }).toList())
+    //println(streamOf(1,2,3,4,5).zipAll(streamOf(10,11,12)).toList())
+    //TODO:做到了 练习 5.13 的 zipAll
 
 //    println(s2.toList())
     //println(streamOf(1,2,3,4).exists { it == 2 })
